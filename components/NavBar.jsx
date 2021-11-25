0de0c2782f38c5dd6463798bd2421a62793a5423
isidrosegui <isidrosegui@gmail.com>
2021-11-25 13:42:52 -0300

    SporteateMarket Segui Weisburd
    Agregado de navbar

---
+++ components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+const NabBar = () => {
+    return (
+        <div>
+           <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">SPORTEATE MARKET</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <NavDropdown title="Deportistas" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Ofertas</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.2">Indumentaria</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Paletas/Raquetas</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+        </div>
+    )
+}
+
+export default NabBar
+
