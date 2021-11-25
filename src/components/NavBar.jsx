d1a20d152751ae89a79da465ecb1077233b4ff51
isidrosegui <isidrosegui@gmail.com>
2021-11-25 13:31:26 -0300

    Add files via upload

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+const NabBar = () => {
+    return (
+        <div>
+           <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">SPORTEATE MARKET</Navbar.Brand>
+  </Container>
+</Navbar>
+        </div>
+    )
+}
+
+export default NabBar
+
