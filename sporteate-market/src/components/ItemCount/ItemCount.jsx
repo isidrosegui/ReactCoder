75dfad32b5a3d10323ca96a71d8c834704c75384
isidrosegui <isidrosegui@gmail.com>
2021-12-06 11:53:53 -0300

    Entrega desafio 4 contador

--- sporteate-market/src/components/ItemCount/ItemCount.jsx
+++ sporteate-market/src/components/ItemCount/ItemCount.jsx
@@ -2,23 +2,35 @@ import Button from '@restart/ui/esm/Button'
 import React from 'react'
 import { FormControl, InputGroup } from 'react-bootstrap'
 import NumericInput from 'react-numeric-input';
-
+import { useState, useEffect } from 'react';
 function ItemCount({stock}) {
-    
-   function addToCart() {
-        let a = 1;
+let cantidad = 0;    
+    const [cantEnCarro, setcantEnCarro] = useState(cantidad);
+
+   function setCantidad(value){
+    cantidad = value;
+   }
+
+    function addToCart(value) {
+       setcantEnCarro(cantidad);
+       //aca debe ser una llamada asincronica para el cant en carro del log aparezca actualizada la primera vez que clickeo 
+       console.log({cantEnCarro})
     }
 
     return (
-        <div  className='row' style={{marginTop:'10px'}}>
-            <div className='col-6'></div>
-            <div className='col-3'>
-            <NumericInput mobile strict className="form-control" style={{width: '200px'}} max = {stock}  />
+        <>
+           
+            <div  className='row' style={{marginTop:'10px'}}>
+                <div className='col-6'></div>
+                <div className='col-3'>
+                    <NumericInput  mobile strict className="form-control" style={{width: '200px'}} max = {stock} min = {0}
+                    defaultValue = {1}  onChange={(value) => setCantidad(value)}/>
+                </div>
+                <div className='col-2'>
+                <Button className='form-control'  onClick={addToCart}>Agregar al Carrito</Button>
+                </div>
             </div>
-            <div className='col-2'>
-            <Button className='form-control' onClick = {addToCart} >Agregar al Carrito</Button>
-            </div>
-        </div>
+        </>
     )
 }
 
