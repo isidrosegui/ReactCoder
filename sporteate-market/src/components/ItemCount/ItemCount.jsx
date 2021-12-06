6bffecd8bcd39449d4193b8ab552e3c19352b53e
isidrosegui <isidrosegui@gmail.com>
2021-12-06 12:02:10 -0300

    detalles 4ta entrega

--- sporteate-market/src/components/ItemCount/ItemCount.jsx
+++ sporteate-market/src/components/ItemCount/ItemCount.jsx
@@ -4,24 +4,25 @@ import { FormControl, InputGroup } from 'react-bootstrap'
 import NumericInput from 'react-numeric-input';
 import { useState, useEffect } from 'react';
 function ItemCount({stock}) {
-let cantidad = 0;    
-    const [cantEnCarro, setcantEnCarro] = useState(cantidad);
+let cantidad = 1;    
+    const [cantEnCarro, setcantEnCarro] = useState(cantidad = 0);
 
    function setCantidad(value){
     cantidad = value;
    }
 
     function addToCart(value) {
+        
        setcantEnCarro(cantidad);
        //aca debe ser una llamada asincronica para el cant en carro del log aparezca actualizada la primera vez que clickeo 
-       console.log({cantEnCarro})
+       console.log('cantencarro'+ cantEnCarro )
     }
 
     return (
         <>
            
             <div  className='row' style={{marginTop:'10px'}}>
-                <div className='col-6'></div>
+                <div className='col-6'><h1>En Carro: {cantEnCarro}</h1></div>
                 <div className='col-3'>
                     <NumericInput  mobile strict className="form-control" style={{width: '200px'}} max = {stock} min = {0}
                     defaultValue = {1}  onChange={(value) => setCantidad(value)}/>
